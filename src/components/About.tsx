2def59a9d0734f489a6af37b2b2614083986284a
QuentinVigier <vigierquentinpro@gmail.com>
2024-07-05 15:23:36 +0200

    Correction images

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -25,8 +25,7 @@ function About() {
 
                     <div className="row">
                         <div className="col-lg-4">
-                        <img src={`${process.env.PUBLIC_URL}/assets/img/profile-img.jpg`} className="img-fluid" alt="Profil Picture" />
-
+                            <img src="./assets/img/profile-img.jpg" className="img-fluid" alt=""></img>
                         </div>
                         <div className="col-lg-8 pt-4 pt-lg-0 content">
                             <h3>UI/UX Designer &amp; Web Developer.</h3>
